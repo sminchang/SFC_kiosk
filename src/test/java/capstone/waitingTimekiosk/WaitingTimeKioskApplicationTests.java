ac793f5f5e246644a537cacab37ec09bb2d24604
Sung Minchang <58043537+sminchang@users.noreply.github.com>
2024-04-01 16:24:45 +0900

    Add files via upload

---
+++ src/test/java/capstone/waitingTimekiosk/WaitingTimeKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.waitingTimekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingTimeKioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1af53077c65f4644b048e4ffd774411b887d50af
sminchang <tjdalsckd0@gmail.com>
2024-04-01 16:53:17 +0900

    First Commit

--- src/main/java/capstone/waitingTimekiosk/WaitingTimeKioskApplication.java
+++ src/main/java/capstone/waitingTimekiosk/WaitingTimeKioskApplication.java
@@ -3,8 +3,9 @@ package capstone.waitingTimekiosk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//https://velog.io/@kbk3771/Spring-Boot-%EC%B9%B4%EC%B9%B4%EC%98%A4-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0 : 카카오 로그인 참조
-//https://innovation123.tistory.com/181: 카카오 로그인 참조
+//카카오 로그인 참조: https://velog.io/@kbk3771/Spring-Boot-%EC%B9%B4%EC%B9%B4%EC%98%A4-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
+//카카오 로그인 참조: https://innovation123.tistory.com/181
+//git 사용법: https://juyoungit.tistory.com/649
 @SpringBootApplication
 public class WaitingTimeKioskApplication {
 

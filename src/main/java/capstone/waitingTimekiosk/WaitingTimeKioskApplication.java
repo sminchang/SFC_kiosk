e91758afe10ff5d8575c765383934d059ba5549d
sminchang <tjdalsckd0@gmail.com>
2024-04-12 00:27:51 +0900

    add timeSetting

--- src/main/java/capstone/waitingTimekiosk/WaitingTimeKioskApplication.java
+++ src/main/java/capstone/waitingTimekiosk/WaitingTimeKioskApplication.java
@@ -3,6 +3,7 @@ package capstone.waitingTimekiosk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//springboot,JPA,thymeleaf 사용법: 인프런,김영한 강의 시리즈
 //카카오 로그인 참조: https://velog.io/@kbk3771/Spring-Boot-%EC%B9%B4%EC%B9%B4%EC%98%A4-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
 //카카오 로그인 참조: https://innovation123.tistory.com/181
 //git 사용법: https://juyoungit.tistory.com/649

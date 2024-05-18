4f5c88934c2209181ba70899cb02e0a8f4c0e0ab
sminchang <tjdalsckd0@gmail.com>
2024-05-19 00:22:07 +0900

    update a menuDemand.html (this month, this week, today service).

--- src/main/resources/static/bottstrap-5.2.3/dist/js/dashboard.js
+++ src/main/resources/static/bottstrap-5.2.3/dist/js/dashboard.js
@@ -17,22 +17,96 @@ function calculateMonthlyData(filteredOrderItems) {
   return monthlyData;
 }
 
-window.renderChart = function (filteredOrderItems) {
-  // 기존 차트 제거
-  var chartContainer = document.getElementById('myChart');
-  var chartCanvas = document.createElement('canvas');
-  chartCanvas.id = 'myChart';
-  chartContainer.parentNode.replaceChild(chartCanvas, chartContainer);
+// 주차별 수량 계산 함수 추가
+function calculateWeeklyData(filteredOrderItems) {
+    var weeklyData = {};
 
-  // 월별 누적 수량 계산
-  var monthlyData = calculateMonthlyData(filteredOrderItems);
+    filteredOrderItems.forEach(function(orderItem) {
+        var dateStr = orderItem.orderTime;
+        if (typeof dateStr === 'string') {
+            var date = new Date(dateStr);
+            var weekNumber = Math.ceil((date.getDate() - 1) / 7);
+            var weekKey = 'Week ' + weekNumber;
+            if (!weeklyData[weekKey]) {
+                weeklyData[weekKey] = 0;
+            }
+            weeklyData[weekKey] += orderItem.quantity;
+        }
+    });
 
-  // 차트 데이터 준비
-  var labels = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'];
-  var data = labels.map(function(label) {
-      var month = label.slice(0, -1);
-      return monthlyData[month] || 0;
-  });
+    return weeklyData;
+}
+
+// 요일별 수량 계산 함수 추가
+function calculateDailyData(filteredOrderItems) {
+    var dailyData = {
+        'Sun': 0, 'Mon': 0, 'Tue': 0, 'Wed': 0, 'Thu': 0, 'Fri': 0, 'Sat': 0
+    };
+
+    filteredOrderItems.forEach(function(orderItem) {
+        var dateStr = orderItem.orderTime;
+        if (typeof dateStr === 'string') {
+            var date = new Date(dateStr);
+            var dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][date.getDay()];
+            dailyData[dayOfWeek] += orderItem.quantity;
+        }
+    });
+
+    return dailyData;
+}
+
+//오늘, 2시간 단위 수량 계산 함수 추가
+function calculateHourlyData(filteredOrderItems) {
+    var hourlyData = {};
+
+    filteredOrderItems.forEach(function(orderItem) {
+        var dateStr = orderItem.orderTime;
+        if (typeof dateStr === 'string') {
+            var date = new Date(dateStr);
+            var hour = Math.floor(date.getHours() / 2) * 2;
+            var hourKey = hour + ':00 - ' + (hour + 2) + ':00';
+            if (!hourlyData[hourKey]) {
+                hourlyData[hourKey] = 0;
+            }
+            hourlyData[hourKey] += orderItem.quantity;
+        }
+    });
+
+    return hourlyData;
+}
+
+window.renderChart = function (filteredOrderItems, option) {
+// 기존 차트 제거
+    var chartContainer = document.getElementById('myChart');
+    var chartCanvas = document.createElement('canvas');
+    chartCanvas.id = 'myChart';
+    chartContainer.parentNode.replaceChild(chartCanvas, chartContainer);
+
+    var data;
+    var labels;
+
+    if (option === 'This month') {
+        var weeklyData = calculateWeeklyData(filteredOrderItems);
+        labels = Object.keys(weeklyData);
+        data = Object.values(weeklyData);
+    } else if (option === 'This week') {
+        var dailyData = calculateDailyData(filteredOrderItems);
+        labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        data = labels.map(function(label) {
+            return dailyData[label] || 0;
+        });
+    } else if (option === 'Today') {
+        var hourlyData = calculateHourlyData(filteredOrderItems);
+        labels = Object.keys(hourlyData);
+        data = Object.values(hourlyData);
+    } else {
+        var monthlyData = calculateMonthlyData(filteredOrderItems);
+        labels = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'];
+        data = labels.map(function(label) {
+            var month = label.slice(0, -1);
+            return monthlyData[month] || 0;
+        });
+    }
 
   // 차트 그리기
   var ctx = document.getElementById('myChart').getContext('2d');
@@ -41,7 +115,9 @@ window.renderChart = function (filteredOrderItems) {
       data: {
           labels: labels,
           datasets: [{
-              label: '월별 누적 수량',
+              label: option === 'This month' ? '주차별 누적 수량' :
+                  option === 'This week' ? '요일별 누적 수량' :
+                      option === 'Today' ? '2시간 단위 누적 수량' : '월별 누적 수량',
               data: data,
               lineTension: 0,
               backgroundColor: 'transparent',
